c80a7586e13554ba0c34aebabc1d48c9404ee130
Sithspitz <rjb590@bham.ac.uk>
2019-01-24 12:37:24 +0000

    jonas

--- ReactomePA_script.R
+++ ReactomePA_script.R
@@ -1,15 +1,38 @@
-# Install and load required packages:
+# Load required base packages:
 
-if (!requireNamespace("devtools", quietly = T))
-  install.packages("devtools")
-  library("devtools")
+library("devtools")
+library("urltools")
 
-if (!requireNamespace("urltools", quietly - T))
-  install.packages("urltools")
-  library("urltools")
-
-
-# Install ReactomePA from Bioconductor:
+# Install ReactomePA from GitHub:
 
 if (!requireNamespace("ReactomePA", quietly = TRUE))
    devtools::install_github(c("GuangchuangYu/DOSE", "GuangchuangYu/ReactomePA"))
+
+# Install clusterProfiler:
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("clusterProfiler", version = "3.8")
+
+# Load ReactomePA and clusterProfiler:
+
+library("ReactomePA")
+library("clusterProfiler")
+library("data.table")
+
+# Import Data:
+## Change Filepath if needed
+
+pre_collated_TCGA_enrichment_data <- read.csv("~/R/R Local Projects/friendly-octo-sniffle/pathfindR_PA/collated_TCGA_enrichment_data.csv")
+
+# Convert Gene ID Symbol to Entrez Gene ID:
+
+df1 <- pre_collated_TCGA_enrichment_data[,1, drop = F]
+df2 <- transpose(df1)
+df3 <- bitr(df2, 
+                             fromType = "SYMBOL", OrgDb = "org.Hs.eg.db", 
+                             toType = "ENTREZID")
+entrez_TCGA_enriched <- df3[,2, drop = F]
+
+
+
