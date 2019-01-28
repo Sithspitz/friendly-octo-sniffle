b97a14c8e17a679ba9f2da17891028895f7acf20
Sithspitz <rjb590@bham.ac.uk>
2019-01-28 15:39:15 +0000

    DLBCL commit

--- pathfindR_PA/pathfindR_script_Reactome_DLBCL_Test.R
+++ pathfindR_PA/pathfindR_script_Reactome_DLBCL_Test.R
@@ -22,16 +22,16 @@ lapply(packages, FUN = function(X) {
 # Import Data:
 ## Change File Path ##
 
-collated_TCGA_enrichment_data <- read.csv("~/R/R Local Projects/friendly-octo-sniffle/pathfindR_PA/collated_TCGA_enrichment_data.csv")
+DLBCL_test <- read.csv("~/R/R Local Projects/friendly-octo-sniffle/pathfindR_PA/DLBCL_test.csv")
 
 # Pathway Enrichment Analysis:
 ## Change File Name ##
 ### Change the p_val_threshold back down to an appropriate level? ###
 #### search_method = SA or GA?!? ####
 
-PEA_results <- run_pathfindR(collated_TCGA_enrichment_data, gene_sets = "Reactome", p_val_threshold = 0.7,
+PEA_results <- run_pathfindR(DLBCL_test, gene_sets = "Reactome", p_val_threshold = 0.7,
                              enrichment_threshold = 0.1, sig_gene_thr = 0,
-                             adj_method = "BH", list_active_snw_genes = T, output_dir = "PEA_Reactome_Results")
+                             adj_method = "BH", list_active_snw_genes = T, output_dir = "PEA_DLBCL_test")
 
 ## Error: Error in grDevices::png(paste0("pathway_visualizations/", current_row$Pathway,  : 
 ## unable to start png() device
